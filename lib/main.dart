a0519a49ed9510f53276ffeeb8820d3810ef1860
joyel <geolrco124@hotmail.com>
2023-02-27 22:01:53 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo_components/pages/home_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(
+
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/pages/list_page.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatelessWidget {
+  List<String> heroes = [
+    "Batman",
+    "Superman",
+    "Wonder Woman",
+    "Flash",
+    "Aquaman",
+  ];
+
+  Map<String, dynamic> teamMap = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+      {
+        "name": "Batman",
+        "image":
+            "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": [
+          "Radiation resistance",
+          "Turning tiny",
+          "Radiation blast",
+        ],
+      },
+      {
+        "name": "Superman",
+        "image":
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes",
+        ],
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+            "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel",
+        ],
+      },
+      {
+        "name": "Batman",
+        "image":
+            "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": [
+          "Radiation resistance",
+          "Turning tiny",
+          "Radiation blast",
+        ],
+      },
+      {
+        "name": "Superman",
+        "image":
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes",
+        ],
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+            "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel",
+        ],
+      },
+      {
+        "name": "Batman",
+        "image":
+            "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": [
+          "Radiation resistance",
+          "Turning tiny",
+          "Radiation blast",
+        ],
+      },
+      {
+        "name": "Superman",
+        "image":
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes",
+        ],
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+            "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel",
+        ],
+      },
+      {
+        "name": "Batman",
+        "image":
+            "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": [
+          "Radiation resistance",
+          "Turning tiny",
+          "Radiation blast",
+        ],
+      },
+      {
+        "name": "Superman",
+        "image":
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes",
+        ],
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+            "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel",
+        ],
+      },
+    ]
+  };
+
+  // showDetailHeroe() {
+  //   showDialog(
+  //     context: context,
+  //     builder: builder,
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text("List Page"),
+      ),
+      // body: ListView.builder(
+      //   itemCount: heroes.length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     print(index);
+      //     return ListTile(
+      //       leading: CircleAvatar(
+      //         child: Text(heroes[index][0]),
+      //       ),
+      //       title: Text(heroes[index]),
+      //     );
+      //   },
+      // ),
+      body: ListView.builder(
+        itemCount: teamMap["members"].length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            onTap: () {
+              print(teamMap["members"][index]);
+              //showDetailHeroe();
+            },
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(teamMap["members"][index]["image"]),
+            ),
+            title: Text(teamMap["members"][index]["name"]),
+            subtitle: Text(teamMap["members"][index]["secretIdentity"]),
+            trailing: const Icon(Icons.view_carousel),
+          );
+        },
+      ),
+    );
+  }
+}
